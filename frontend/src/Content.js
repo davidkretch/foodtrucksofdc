45275326eea3283459aa04fee842df54a69b3538
David Kretch <david.kretch@gmail.com>
2019-07-05 15:42:46 -0400

    Get and display truck ratings

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -1,25 +1,38 @@
 import React from "react";
-import Card from "react-bootstrap/Card"
-import ListGroup from "react-bootstrap/ListGroup"
-import Jumbotron from "react-bootstrap/Jumbotron"
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+import Rating from "./Rating";
+
+function Truck(props) {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between align-items-center">
+      {props.truck.name}
+      <Rating rating={props.truck.rating} key={props.truck.name} />
+    </ListGroup.Item>
+  );
+}
+
+function Stop(props) {
+  return (
+    <div id={props.stop.link} style={{paddingTop: "56px", marginTop: "-56px"}}>
+      <Card className="mt-4 mb-4">
+        <Card.Header as="h5">{props.stop.name}</Card.Header>
+        <ListGroup variant="flush">
+          {props.stop.trucks.map((truck, index) => {
+            return <Truck truck={truck} key={index} />
+          })}
+        </ListGroup>
+      </Card>
+    </div>
+  );
+}
 
 function Stops(props) {
   return (
     props.stops.map(stop => {
-        return (
-          <div id={stop.link} style={{paddingTop: "56px", marginTop: "-56px"}} key={stop.name}>
-            <Card className="mt-4 mb-4">
-              <Card.Header as="h5">{stop.name}</Card.Header>
-              <ListGroup variant="flush">
-                {stop.trucks.map((truck, index) => {
-                  return (
-                    <ListGroup.Item key={index}>{truck}</ListGroup.Item>
-                  )
-                })}
-              </ListGroup>
-            </Card>
-          </div>
-        )
+        return <Stop stop={stop} key={stop.name} />
     })
   );
 }
---
+++ frontend/src/Rating.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+function Star(props) {
+    return (
+        <FontAwesomeIcon icon={faStar} {...props} />
+    );
+}
+
+// ButtonStars acts as the rating buttons for the Rating component.
+// It also shows light gray placeholder stars.
+function ButtonStars(props) {
+    const color = "e8e8e8";
+    var stars = [];
+    for (var i = 1; i <= 3; i++) {
+        stars.push(
+            <Star
+                key={i}
+                onMouseOver={props.onMouseOver.bind(this, i)}
+                onMouseOut={props.onMouseOut}
+                onClick={props.onClick.bind(this, i)}
+                style={{color: color}}
+            />
+        );
+    }
+    return (
+        <span style={{whiteSpace: "nowrap"}}>
+            {stars}
+        </span>
+    );
+}
+
+// DisplayStars shows the rating in stars (either provided or user-selected).
+function DisplayStars(props) {
+    const color = "ffbc0b";
+    const width = Math.floor(100 * props.rating / 3) + "%";
+    return (
+        <div style={{width: width, overflow: "hidden"}}>
+            <span style={{whiteSpace: "nowrap"}}>
+                <Star style={{color: color}} />
+                <Star style={{color: color}} />
+                <Star style={{color: color}} />
+            </span>
+        </div>
+    )
+}
+
+class Rating extends React.Component {
+    constructor(props) {
+        super(props);
+        this.highlight = this.highlight.bind(this);
+        this.clearHighlight = this.clearHighlight.bind(this);
+        this.rate = this.rate.bind(this);
+        this.state = {
+            rating: this.props.rating || 0,
+            selected: null,
+            highlighted: null
+        }
+    }
+
+    highlight(i) {
+        this.setState({
+            highlighted: i
+        });
+    }
+
+    clearHighlight() {
+        this.setState({
+            highlighted: null
+        });
+    }
+
+    rate(i) {
+        this.setState({
+            selected: i
+        });
+    }
+
+    render() {
+        const rating = this.state.highlighted || this.state.selected || this.state.rating;
+
+        const buttonStars = {
+            position: "relative"
+        };
+        const displayStars = {
+            position: "absolute",
+            top: 0,
+            pointerEvents: "none"
+        };
+
+        return (
+            <div>
+                <div style={buttonStars}>
+                    <ButtonStars
+                        onMouseOver={this.highlight}
+                        onMouseOut={this.clearHighlight}
+                        onClick={this.rate}
+                    />
+                    <div style={displayStars}>
+                        <DisplayStars rating={rating} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Rating;
