5449d383191efbe437a4e74a37803ca205ed1fd4
David Kretch <david.kretch@gmail.com>
2019-07-05 18:41:57 -0400

    Add comments

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -2,6 +2,8 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+// Sidebar renders the sidebar with a list of stops, and links for jumping
+// to a specific stop.
 function Sidebar(props) {
     return (
         <Navbar className="d-none d-sm-block mt-1" style={{position: "fixed"}}>
